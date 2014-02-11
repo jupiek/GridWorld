b56757d632454eb34395c0a62ee966869e86b8eb
Alicia Chen <mail4aliciac@gmail.com>
2014-02-10 22:27:46 -0500

    spiral and circlebug

---
+++ GridWorldCode/GridWorldCode/projects/boxBug/SpiralBug.java
@@ -0,0 +1,39 @@
+import info.gridworld.actor.Bug;
+
+public class SpiralBug extends Bug
+{
+    private int steps,side;
+    private int sideLength;
+
+    /**
+     * Constructs a box bug that traces a spiral starting with a given side length
+     * @param length the side length
+     */
+
+
+    public SpiralBug(int length)
+    {
+        steps = 0;
+        sideLength = length;
+    }
+
+    /**
+     * Moves to the next location of the square.
+     */
+
+    public void act()
+    {
+        if (steps < sideLength && canMove())
+        {
+            move();
+            steps++;
+        }
+        else
+        {
+	    turn();
+            turn();
+            steps = 0;
+	    sideLength ++;
+        }
+    }
+}
